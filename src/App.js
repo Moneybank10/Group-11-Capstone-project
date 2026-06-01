1ad19ca296f3c224c12f5a872c7c77c4549abd11
Moneybank10 <folorunsotaofeek2018@gmail.com>
2026-06-01 20:32:23 +0100

    Set up App.js template and remove VideoSection test files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import './App.css';
 
+// Import all components as they are completed by teammates
+// Uncomment each import as the component is ready
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import VideoSection from './components/VideoSection';
+// import PlanetCards from './components/PlanetCards';
+// import PlanetsTable from './components/PlanetsTable';
+// import ContactForm from './components/ContactForm';
+// import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
-      {/* Navbar component will go here */}
-      {/* Hero component will go here */}
-      {/* videosection component will go here */}
-      {/* Planet Cards will go here */}
-      {/* Planets Table will go here */}
-      {/* Contact Form will go here */}
-      {/* Footer will go here */}
+      {/* NAVBAR SECTION */}
+      {/* <Navbar /> */}
+
+      {/* HERO SECTION */}
+      {/* 
+        The Hero component should include two buttons:
+        
+        <button onClick={() => document.getElementById('planets').scrollIntoView({ behavior: 'smooth' })}>
+          Explore the Data
+        </button>
+
+        <button onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })}>
+          Contact Us
+        </button>
+      */}
+      {/* <Hero /> */}
+
+      {/* VIDEO SECTION */}
+      {/* <VideoSection /> */}
+
+      {/* PLANET CARDS SECTION */}
+      {/* This section displays planet cards fetched from API */}
+      <section id="planets">
+        {/* <PlanetCards /> */}
+      </section>
+
+      {/* PLANETS TABLE SECTION */}
+      {/* This section displays planetary facts in a table format */}
+      <section id="planets-table">
+        {/* <PlanetsTable /> */}
+      </section>
+
+      {/* CONTACT FORM SECTION */}
+      {/* This section contains the contact form for user inquiries */}
+      <section id="contact">
+        {/* <ContactForm /> */}
+      </section>
+
+      {/* FOOTER SECTION */}
+      {/* <Footer /> */}
     </div>
   );
 }
